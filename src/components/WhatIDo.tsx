b2ba995df6e0741a4bf3f5eb0e95df2d726af545
Techvenom18 <sumitrathore45528@gmail.com>
2026-08-26 14:20:57 +0530

    Redesign WhatIDo cards: solid colored borders, inline icons, role-based color identity

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import "./styles/WhatIDo.css";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { config } from "../config";
+import { FiDatabase, FiCode } from "react-icons/fi";
 
 const WhatIDo = () => {
   const containerRef = useRef<(HTMLDivElement | null)[]>([]);
@@ -37,102 +38,54 @@ const WhatIDo = () => {
       </div>
       <div className="what-box">
         <div className="what-box-in">
-          <div className="what-border2">
-            <svg width="100%">
-              <line
-                x1="0"
-                y1="0"
-                x2="0"
-                y2="100%"
-                stroke="white"
-                strokeWidth="2"
-                strokeDasharray="7,7"
-              />
-              <line
-                x1="100%"
-                y1="0"
-                x2="100%"
-                y2="100%"
-                stroke="white"
-                strokeWidth="2"
-                strokeDasharray="7,7"
-              />
-            </svg>
-          </div>
           <div
-            className="what-content what-noTouch"
+            className="what-content what-noTouch what-card-data"
             ref={(el) => setRef(el, 0)}
           >
-            <div className="what-border1">
-              <svg height="100%">
-                <line
-                  x1="0"
-                  y1="0"
-                  x2="100%"
-                  y2="0"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeDasharray="6,6"
-                />
-                <line
-                  x1="0"
-                  y1="100%"
-                  x2="100%"
-                  y2="100%"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeDasharray="6,6"
-                />
-              </svg>
-            </div>
-            <div className="what-corner"></div>
-
             <div className="what-content-in">
-              <h3>{config.skills.develop.title}</h3>
+              <div className="what-heading-row">
+                <div className="what-icon-badge what-icon-badge-data">
+                  <FiDatabase />
+                </div>
+                <h3>{config.skills.develop.title}</h3>
+              </div>
               <h4>{config.skills.develop.description}</h4>
-              <p>
-                {config.skills.develop.details}
-              </p>
+              <p>{config.skills.develop.details}</p>
               <h5>Skillset & tools</h5>
               <div className="what-content-flex">
                 {config.skills.develop.tools.map((tool, index) => (
                   <div key={index} className="what-tags">{tool}</div>
                 ))}
               </div>
-              <div className="what-arrow"></div>
+              <div
+                className="what-arrow"
+                onClick={() => handleClick(containerRef.current[0]!)}
+              ></div>
             </div>
           </div>
           <div
-            className="what-content what-noTouch"
+            className="what-content what-noTouch what-card-code"
             ref={(el) => setRef(el, 1)}
           >
-            <div className="what-border1">
-              <svg height="100%">
-                <line
-                  x1="0"
-                  y1="100%"
-                  x2="100%"
-                  y2="100%"
-                  stroke="white"
-                  strokeWidth="2"
-                  strokeDasharray="6,6"
-                />
-              </svg>
-            </div>
-            <div className="what-corner"></div>
             <div className="what-content-in">
-              <h3>{config.skills.design.title}</h3>
+              <div className="what-heading-row">
+                <div className="what-icon-badge what-icon-badge-code">
+                  <FiCode />
+                </div>
+                <h3>{config.skills.design.title}</h3>
+              </div>
               <h4>{config.skills.design.description}</h4>
-              <p>
-                {config.skills.design.details}
-              </p>
+              <p>{config.skills.design.details}</p>
               <h5>Skillset & tools</h5>
               <div className="what-content-flex">
                 {config.skills.design.tools.map((tool, index) => (
                   <div key={index} className="what-tags">{tool}</div>
                 ))}
               </div>
-              <div className="what-arrow"></div>
+              <div
+                className="what-arrow"
+                onClick={() => handleClick(containerRef.current[1]!)}
+              ></div>
             </div>
           </div>
         </div>
